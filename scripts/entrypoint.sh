23240ed0c1ede6d53788a0c590aed003899f8063
Jarkko Kallio <jarkko.kallio@hoxhunt.com>
2023-10-17 14:04:07 +0300

    store generated keys in a docker volume

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,6 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
-$SCRIPT_PATH/generate-certificates.sh $(pwd) idp
+$SCRIPT_PATH/generate-certificates.sh /application/keys idp
 
 yarn start
--- src/startServer.js
+++ src/startServer.js
@@ -4,5 +4,7 @@ const config = require("./config");
 runServer({
   acsUrl: `https://foo.okta.com/auth/saml20/assertion-consumer`,
   audience: `https://foo.okta.com/auth/saml20/metadata`,
+  cert: "./keys/idp-public-cert.pem",
+  key: "./keys/idp-private-key.pem",
   ...config,
 });
